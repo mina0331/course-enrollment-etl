55b4f16c2caba13e7a44ee103085f05458e3da9f
Somin Ahn <sominahn@Somins-MacBook-Air.local>
2025-12-28 01:46:10 -0500

    set up the apache airflow (batch processing)

---
+++ dockerfile
@@ -0,0 +1,8 @@
+FROM apache/airflow:latest
+
+USER root
+RUN apt-get update && \
+    apt-get install -y git && \
+    apt-get clean
+
+USER airflow
--- mongo_db_driver.py
+++ mongo_db_driver.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://ahnsomin638_db_user:EFpp7D0l5DWFzled@cluster0.li8yaus.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://ahnsomin638_db_user:d21yhZEcnKwSdexG@cluster0.li8yaus.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
